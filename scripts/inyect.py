b12b24a28f2001d1a6c32aba6a42e2ac7a4e97c4
Ariel <amj19193@hotmail.com>
2026-09-19 20:17:53 -0300

    feat: inyect data script

---
+++ scripts/inyect.py
@@ -0,0 +1,30 @@
+from scripts.transform import transform_data, cleaning_data
+from scripts.extract import get_data
+import requests
+from src.api.db.config import settings
+
+
+def api_inyection(data, create_endpoint, headers):
+
+    payload = data.to_dict(orient="records")
+    response = requests.post(create_endpoint, headers=headers, json=payload)
+    print(f"Status Code: {response.status_code}")
+    print(f"Response: {response.text}")
+    
+    
+    
+if __name__ == "__main__":
+    
+    base_url = "https://analytics-api-hg65.onrender.com"
+    path = "/api/events/"
+    create_endpoint = f"{base_url}{path}"
+    
+    headers = {
+        "X-API-KEY": settings.AUTOMATION_API_KEY,
+        "content-type": "application/json"
+    }
+    
+    data = get_data()
+    df = transform_data(data)
+    df_clean = cleaning_data(df)
+    api_inyection(df_clean, create_endpoint, headers)
