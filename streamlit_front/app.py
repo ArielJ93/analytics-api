315117e6d4d9680a5a76fc7f0a69fc978b193298
Ariel <amj19193@hotmail.com>
2026-09-24 17:39:10 -0300

    feat: add bubble chart to multiple coins selection

--- streamlit_front/app.py
+++ streamlit_front/app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 
-main_page = st.Page("pages/1_home.py", title="Hello", icon="🎈")
-page_2 = st.Page("pages/2_dashboard.py", title="Page 2", icon="❄️")
+main_page = st.Page("pages/1_home.py", title="Main", icon="🎈")
+page_2 = st.Page("pages/2_dashboard.py", title="Dashboard", icon="❄️")
 page_3 = st.Page("pages/3_AI.py", title="Page 3", icon="🤖")
-page_4 = st.Page("pages/4_feedback.py", title="Page 4", icon="🎉")
+
 
 
 # Set up navigation
-pg = st.navigation([main_page, page_2, page_3, page_4])
+pg = st.navigation([main_page, page_2, page_3])
 
 # Run the selected page
 pg.run()
--- streamlit_front/pages/2_dashboard.py
+++ streamlit_front/pages/2_dashboard.py
@@ -43,10 +43,16 @@ def get_api_data(url, params, headers):
 
 with st.sidebar:
     st.sidebar.header("Dashboard V1")
-    symbol_filter = st.multiselect('Select the crypto you want to visualize:', 
+    selection_mode = st.radio("Crypto selection", options=["One coin", "Multiple coins"])
+    if selection_mode == "One coin":
+        symbol_filter = st.multiselect('Select the crypto you want to visualize:', 
                 options= params['symbol'],
                 default= ['btc'],
                 max_selections=1)
+    if selection_mode == "Multiple coins":
+        symbol_filter = st.multiselect('Select cryptos to compare:', 
+                    options= params['symbol'],
+                    default= ['m', 'dot', 'render', 'atom'])
     st.divider()
     duration_filter = st.pills("Select the interval values:", options=["15 minutes","1 hour", "4 hours", "1 day"], default= "15 minutes")
     st.divider()
@@ -71,18 +77,20 @@ else:
     with st.container(border=True):   
         with st.spinner("Fetching data from API...", show_time=True):
             df = get_api_data(url, filter_params, headers) 
-            metric_1, metric_2, metric_3 = st.columns(3)
-            with metric_1:
-                #delta is the percentage change between the first row  and the last row, the time interval between the first row and the last one represent the interval selected on the history filter.
-                delta_price = ((df['avg_price'].iloc[-1] - df['avg_price'].iloc[1]) / df['avg_price'].iloc[1]) * 100
-                st.metric("PRICE", value=df['avg_price'].iloc[-1], delta=f"{delta_price:.2f}%")
-            with metric_2:
-                delta_volume = ((df['volume_24h'].iloc[-1] - df['volume_24h'].iloc[1]) / df['volume_24h'].iloc[1]) * 100
-                st.metric("VOLUME 24H", value=df['volume_24h'].iloc[-1], delta=f"{delta_volume:.2f}%")
-            with metric_3:
-                delta_market = ((df['market_cap'].iloc[-1] - df['market_cap'].iloc[1]) / df['market_cap'].iloc[1]) * 100
-                st.metric("MARKET CAP", value=df['market_cap'].iloc[-1], delta=f"{delta_market:.2f}%")
-            line_graph_tab, df_tab = st.tabs(["Linear Graph", "Complete DataFrame"])
+            if len(symbol_filter) == 1:
+                metric_1, metric_2, metric_3 = st.columns(3)
+                with metric_1:
+                    #delta is the percentage change between the first row  and the last row, the time interval between the first row and the last one represent the interval selected on the history filter.
+                    delta_price = ((df['avg_price'].iloc[-1] - df['avg_price'].iloc[1]) / df['avg_price'].iloc[1]) * 100
+                    st.metric("PRICE  $USD", value=df['avg_price'].iloc[-1], delta=f"{delta_price:.2f}%")
+                with metric_2:
+                    delta_volume = ((df['volume_24h'].iloc[-1] - df['volume_24h'].iloc[1]) / df['volume_24h'].iloc[1]) * 100
+                    st.metric("VOLUME 24H", value=df['volume_24h'].iloc[-1], delta=f"{delta_volume:.2f}%")
+                with metric_3:
+                    delta_market = ((df['market_cap'].iloc[-1] - df['market_cap'].iloc[1]) / df['market_cap'].iloc[1]) * 100
+                    st.metric("MARKET CAP", value=df['market_cap'].iloc[-1], delta=f"{delta_market:.2f}%")
+                
+            line_graph_tab, bubble_chart, heat_map, df_tab = st.tabs(["Linear Graph", "Bubble Chart", "Heat Map", "Complete DataFrame"])
             with line_graph_tab:
                 fig = px.line(df, x='bucket', y='avg_price', color='symbol')
                 fig.update_layout(
@@ -98,8 +106,46 @@ else:
                                         )),
                     yaxis=dict(
                         title=dict(
-                            text="Price"
+                            text="Price $USD"
                                 )))
                 st.plotly_chart(fig)
+            if len(symbol_filter) > 1:
+                with bubble_chart:
+                    animation = st.checkbox(label="Animation of data history")
+                    if animation:
+                        fig = px.scatter(
+                                    df, x="change_24h", y="volume_24h",
+                                    size="market_cap", color="symbol",
+                                    hover_name="symbol",
+                                    log_y=True,
+                                    size_max=60,
+                                    animation_frame="bucket",
+                                    animation_group="symbol")
+                        fig.update_layout(
+                            title={
+                                'text': 'Volume vs Percentual Change (24h)',
+                                'y':0.9,
+                                'x':0.5,
+                                'xanchor': 'center',
+                                'yanchor': 'top'})
+                        st.plotly_chart(fig)
+                    else:
+                        last_date = df['bucket'].iloc[-1]
+                        df_bubble_chart = df[df['bucket'] == last_date]
+                        fig = px.scatter(df_bubble_chart, x="change_24h", y="volume_24h",
+                                size="market_cap", color="symbol",
+                                hover_name="symbol",
+                                log_y=True,
+                                size_max=60,
+                                )
+                        fig.update_layout(
+                            title={
+                                'text': 'Volume vs Percentual Change (24h) <br><sup>*Bubble size represents Market Cap</sup>',
+                                'y':0.9,
+                                'x':0.5,
+                                'xanchor': 'center',
+                                'yanchor': 'top'})
+                        st.plotly_chart(fig)
             with df_tab:
                 st.dataframe(df)
+        
